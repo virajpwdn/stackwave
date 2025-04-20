7bf4a6852a54d930aeebe26d03cf0155e0e73b16
virajpwdn <virajpatwardhan6@gmail.com>
2025-04-20 18:59:18 +0530

    BUG: Route name change of question to create-question

--- Server/src/router/questions.routes.js
+++ Server/src/router/questions.routes.js
@@ -3,7 +3,7 @@ const questionRouter = Router();
 const controller = require("../controller/question.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
-questionRouter.post("/question", controller.askQuestionController);
+questionRouter.post("/create-question", controller.askQuestionController);
 questionRouter.get(
   "/view/question/:questionId",
   controller.viewQuestionController
