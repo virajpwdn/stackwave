79e99f7969aa87675506d7eabfafe76bfe86aacc
virajpwdn <virajpatwardhan6@gmail.com>
2025-05-01 06:56:55 +0530

    updating prod urls

--- Server/src/app.js
+++ Server/src/app.js
@@ -14,7 +14,7 @@ const compilerRoutes = require("../src/router/compiler.routes");
 const roomRoutes = require("../src/router/room.routes");
 const messageRoutes = require("../src/router/message.routes");
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://stackwave-frontend-ejbk.onrender.com", credentials: true }));
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
