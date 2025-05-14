8f332325c9aabfb201ca5e53b948e3e3ec856b1c
virajpwdn <virajpatwardhan6@gmail.com>
2025-05-15 04:13:05 +0530

    created new brach && done setup of nginx && docker-compose file created

--- Client/src/config/baseurl.js
+++ Client/src/config/baseurl.js
@@ -1,6 +1,7 @@
-export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:3000/api/v1"
-    : "https://stackwave-vy6f.onrender.com/api/v1";
+// export const BASE_URL =
+//   location.hostname === "localhost"
+//     ? "http://localhost:3000/api/v1"
+//     : "https://stackwave-vy6f.onrender.com/api/v1";
 
 // export const BASE_URL = "http://localhost:3000/api/v1"
+export const BASE_URL = "/api"
