d6f6ea2f32f59cc88d75a7604f09003fac88cd12
virajpwdn <virajpatwardhan6@gmail.com>
2025-05-17 04:22:56 +0530

    working on dynamic URL

--- Client/src/config/baseurl.js
+++ Client/src/config/baseurl.js
@@ -1,7 +1,7 @@
-// export const BASE_URL =
-//   location.hostname === "localhost"
-    // ? "http://localhost:3000/api/v1"
-//     : "https://stackwave-vy6f.onrender.com/api/v1";
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "/api/api/v1"
+    : "https://stackwave-vy6f.onrender.com/api/v1";
 
 // export const BASE_URL = "http://localhost:3000/api/v1"
-export const BASE_URL = "/api/v1"
+// export const BASE_URL = "/api/v1"
--- Client/src/utils/socket.js
+++ Client/src/utils/socket.js
@@ -5,7 +5,14 @@ import { io } from "socket.io-client";
 //     ? "http://localhost/api"
 //     : "https://stackwave-vy6f.onrender.com";
 
-const socketConnectionURI = "http://localhost";
+// const socketConnectionURI = "http://localhost";
+
+let socketConnectionURI;
+if (location.hostname === "localhost") {
+  socketConnectionURI = ["http://localhost", "http://localhost/api"];
+} else {
+  socketConnectionURI = "https://stackwave-vy6f.onrender.com";
+}
 
 const socket = io(socketConnectionURI, {
   transports: ["websocket", "polling"],
