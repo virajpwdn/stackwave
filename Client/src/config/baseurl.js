ad1963d8ed98ca43535aa4aaf76091bed3e8e4c8
virajpwdn <virajpatwardhan6@gmail.com>
2025-05-17 15:35:23 +0530

    url changes

--- Client/src/config/baseurl.js
+++ Client/src/config/baseurl.js
@@ -1,9 +1,9 @@
 //! here also url has been change so might affect container
 
-export const BASE_URL =
-  location.hostname === "localhost"
-    ? "/api/api/v1"
-    : "https://stackwave-vy6f.onrender.com/api/v1";
+// export const BASE_URL =
+//   location.hostname === "localhost"
+//     ? "/api/api/v1"
+//     : "https://stackwave-vy6f.onrender.com/api/v1";
 
-// export const BASE_URL = "http://localhost:3000/api/v1"
+export const BASE_URL = "http://localhost:3000/api/v1"
 // export const BASE_URL = "/api/v1"
--- Client/src/utils/socket.js
+++ Client/src/utils/socket.js
@@ -1,19 +1,12 @@
 import { io } from "socket.io-client";
 
-// const socketConnectionURI =
-//   location.hostname === "localhost"
-//     ? "http://localhost/api"
-//     : "https://stackwave-vy6f.onrender.com";
+const socketConnectionURI =
+  location.hostname === "localhost"
+    ? "http://localhost/api"
+    : "https://stackwave-vy6f.onrender.com";
 
 // const socketConnectionURI = "http://localhost";
 
-//! This files have been changed thats why container is not working
-let socketConnectionURI;
-if (location.hostname === "localhost") {
-  socketConnectionURI = ["http://localhost", "http://localhost/api"];
-} else {
-  socketConnectionURI = "https://stackwave-vy6f.onrender.com";
-}
 
 const socket = io(socketConnectionURI, {
   transports: ["websocket", "polling"],
