5a8051c91202ef509de9c280f21877f3948ee2db
virajpwdn <virajpatwardhan6@gmail.com>
2025-12-06 06:36:11 +0530

    feat: get room by user id

--- Server/src/router/room.routes.js
+++ Server/src/router/room.routes.js
@@ -1,7 +1,12 @@
 const {Router} = require("express");
 const roomRouter = Router();
 const controller = require("../controller/room.controller");
+const authMiddleware = require("../middleware/auth.middleware")
 
 roomRouter.get("/all-rooms", controller.getAllRooms)
+/**
+ * /reem-by-id this api will return number of live room created by loggedIn User
+ */
+roomRouter.get("/room-by-id", authMiddleware, controller.getRoomById)
 
 module.exports = roomRouter;
