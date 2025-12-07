61c9830c4629bc1725ee27eb6446979b27eded7b
virajpwdn <virajpatwardhan6@gmail.com>
2025-12-07 16:30:13 +0530

    feat: minor update in room controller

--- Server/src/controller/room.controller.js
+++ Server/src/controller/room.controller.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("../middleware/asyncHandler");
 const RoomModal = require('../models/room.model');
+const AppError = require("../utils/ApiError")
+const AppResponse = require("../utils/ApiResponse")
 
 module.exports.getAllRooms = asyncHandler(async(req,res)=>{
     try {
@@ -8,4 +10,11 @@ module.exports.getAllRooms = asyncHandler(async(req,res)=>{
     } catch (error) {
         console.error(error);
     }
+})
+
+module.exports.getRoomById = asyncHandler(async (req,res) => {
+    const totalRoom = await RoomModal.countDocuments({roomCreatedBy: req.user._id})
+    if(!totalRoom) throw new AppError(400, "No live rooms found")
+    
+    res.status(200).json(new AppResponse(200, totalRoom, "All Rooms Created by youq"))
 })
