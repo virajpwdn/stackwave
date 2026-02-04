c8ff8928c7cf968ef49e8c5c556796f3a0e9bb0f
virajpwdn <virajpatwardhan6@gmail.com>
2026-02-04 19:31:57 +0530

    feat: piechart complete

--- Client/src/ui/settings/ChartBox.jsx
+++ Client/src/ui/settings/ChartBox.jsx
@@ -1,50 +1,6 @@
 import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
 
 const ChartBox = (props) => {
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
   return (
     <div className="chartbox flex h-full">
       <div className="boxInfo flex flex-[3] flex-col justify-between">
@@ -54,7 +10,7 @@ const ChartBox = (props) => {
             alt=""
             className="h-10 w-10 rounded-full"
           />
-          <span>{props.title}</span>
+          <span className="text-sm font-semibold">{props.title}</span>
         </div>
         <h1 className="text-2xl font-bold">{props.number}</h1>
         <p style={{ color: props.color }}>View all</p>
---
+++ Client/src/ui/settings/PieChart.jsx
@@ -0,0 +1,57 @@
+import { Pie, PieChart, Tooltip } from "recharts";
+
+const PieChartGraph = ({ isAnimationActive = true }) => {
+  const data = [
+    { name: "Group A", value: 400, fill: "#0088FE" },
+    { name: "Group B", value: 300, fill: "#00C49F" },
+    { name: "Group C", value: 300, fill: "#FFBB28" },
+    { name: "Group D", value: 200, fill: "#FF8042" },
+  ];
+  return (
+    <div className="flex h-full flex-col justify-between">
+      <h1 className="mb-10 text-xl font-bold">Lead by Source</h1>
+      <div className="chart flex items-center justify-center">
+        <PieChart
+          style={{
+            width: "100%",
+            maxWidth: "500px",
+            maxHeight: "80vh",
+            aspectRatio: 1,
+          }}
+          responsive
+        >
+          <Tooltip
+            contentStyle={{ background: "white", borderRadius: "5px" }}
+          />
+          <Pie
+            data={data}
+            innerRadius="80%"
+            outerRadius="100%"
+            // Corner radius is the rounded edge of each pie slice
+            cornerRadius="50%"
+            fill="#8884d8"
+            // padding angle is the gap between each pie slice
+            paddingAngle={5}
+            dataKey="value"
+            isAnimationActive={isAnimationActive}
+          />
+        </PieChart>
+      </div>
+      <div className="options flex items-center justify-between gap-10 text-sm">
+        {data.map((item) => (
+          <div key={item.name}>
+            <div>
+              <div
+                className="dot h-2 w-2 rounded-[50%]"
+                style={{ backgroundColor: item.fill }}
+              ></div>
+              <span>{item.name}</span>
+            </div>
+            <span>{item.value}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default PieChartGraph;
