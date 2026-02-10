3531121213237cb4139b5f749ffa217adec8cc3f
virajpwdn <virajpatwardhan6@gmail.com>
2026-02-10 17:53:51 +0530

    feat: create dataset for rag imported langchain tools

--- Server/src/router/ai.routes.js
+++ Server/src/router/ai.routes.js
@@ -4,5 +4,6 @@ const controller = require("../controller/ai.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
 aiRouter.post("/generate", authMiddleware, controller.refactorCodeAI);
+aiRouter.get("/indexing", authMiddleware, controller.indexingDocument);
 
 module.exports = aiRouter;
