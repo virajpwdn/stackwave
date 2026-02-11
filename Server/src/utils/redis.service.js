a1e8902c4e06da591e09f3f49af0086fd15fdd58
virajpwdn <virajpatwardhan6@gmail.com>
2026-02-11 17:14:31 +0530

    feat: add async distributed queue

--- Server/src/utils/redis.service.js
+++ Server/src/utils/redis.service.js
@@ -5,18 +5,12 @@ const redis = new Redis({
   port: config.REDIS_PORT,
   host: config.REDIS_HOST,
   password: config.REDIS_PASSWORD,
+
+  maxRetriesPerRequest: null,
 });
 
 redis.on("connect", () => {
   console.log("Redis is connected");
 });
-
-// const setData = async () => {
-//   await redis.set("foo", "bar");
-//   const result = await redis.get("foo");
-//   console.log(result);
-// };
-
-// setData()
 
 module.exports = redis;
